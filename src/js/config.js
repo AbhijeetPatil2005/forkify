7800209cd4ca54e931794a83836763dcede0a42e
Abhijeet Patil <abhijeetpatil032005@gmail.com>
2025-02-08 17:07:32 +0530

    commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL ='https://forkify-api.jonas.io/api/v2/recipes/';
 export const Timeout_SEC =10; 
 export const RES_PER_PAGE = 10;
+export const KEY ='295559f9-b5e0-4884-bca8-051d3460bbae';
+export const MODAL_CLOSE_SEC = 2.5;
